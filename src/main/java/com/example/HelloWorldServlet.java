dadcd92f735793314690751fdd87def1b34cd8c7
aion0721 <rudolf.aion@gmail.com>
2024-03-12 17:52:33 +0900

    get parameter

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,14 +1,25 @@
 package com.example;
 
-import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import java.io.IOException;
 
 public class HelloWorldServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		response.getWriter().println("<h1>Hello, World</h1>");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+		try{
+			response.setContentType("text/html/charset=UTF-8");
+
+			String name = request.getParameter("name");
+			if(name == null || name.isEmpty()) {
+				name = "World";
+			}
+
+			response.getWriter().println("<h1>Hello, " + name + "!</h1>");
+		}catch(IOException e){
+			e.printStackTrace();
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+			
 	}
 }
